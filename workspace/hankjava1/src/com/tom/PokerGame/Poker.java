396bd4dde815cef9f17f66170cd7313265d0ef8f
user <user@es302n19>
2018-12-04 12:02:35 +0800

    poker game

---
+++ workspace/hankjava1/src/com/tom/PokerGame/Poker.java
@@ -0,0 +1,48 @@
+package com.tom.PokerGame;
+
+import java.util.Random;
+
+public class Poker {
+	Card[] cards = new Card[52];
+	//int[] cards = new int[52];
+
+	public Poker() {
+
+		for (int i = 0; i < 52; i++) {
+			cards[i] = new Card(i);
+			Random random = new Random();
+			int r = random.nextInt(52);
+			Card tmp = cards[i];
+			cards[i] = cards[r];
+			cards[r] = tmp;
+					
+		}
+	}
+
+	public void print() {
+		for (int i = 0; i < 52; i++) {
+			/*int flower = cards[i % 13];
+			char c = 0;
+			switch (flower) {
+			case 0:
+				c = 'C';
+				break;
+			case 1:
+				c = 'H';
+				break;
+			case 2:
+				c = 'D';
+				break;
+			case 3:
+				c = 'S';
+				break;
+			}
+			System.out.println(c);
+*/
+			System.out.print(cards[i].get() + " ");
+			if (i % 13 == 12)
+				System.out.println();
+		}
+	}
+
+}
--- workspace/hankjava1/src/com/tom/PokerGame/PokerGame.java
+++ workspace/hankjava1/src/com/tom/PokerGame/PokerGame.java
@@ -1,42 +1,53 @@
 package com.tom.PokerGame;
 
-import java.util.Random;
 
 public class PokerGame {
-	public static void main(String[] args) {
-		int[] f = {0x2660,0x2663,0x2665,0x2666};
-		System.out.println(f);
-		int nums[] = new int[5];
-		nums[0] = 77;
-		nums[4] = 98;
-		System.out.println(nums[4]);
-			System.out.println(nums.length);
-		
-		for (int i = 0; i < nums.length; i++) {
-			System.out.println(nums[i]);
-		}	
-		Random random = new Random();
-		for (int i = 0; i < 52; i++) {
-			int number = random.nextInt(13) + 1;
-			int flower = random.nextInt(4);
-			System.out.print(number);
-			char c = 0;
-			switch (flower) {
-			case 0:
-				c = 'C';
-				break;
-			case 1:
-				c = 'H';
-				break;
-			case 2:
-				c = 'D';
-				break;
-			case 3:
-				c = 'S';
-				break;
-			}
-			System.out.println(c);
-
-		}
+	
+public static void main(String[] args) {
+	int[] n = new int[10];	
+	Card[] c = new Card[10];
+	System.out.println(n[0]);
+	c[0] = new Card(2);
+	System.out.println(c[0].get());
+	
+		Poker poker = new Poker();
+		poker.print();
 	}
 }
+
+/*
+int nums[] = new int[5];
+nums[0] = 77;
+nums[4] = 98;
+System.out.println(nums[4]);
+	System.out.println(nums.length);
+
+for (int i = 0; i < nums.length; i++) {
+	System.out.println(nums[i]);
+}	
+Random random = new Random();
+for (int i = 0; i < 52; i++) {
+	int number = random.nextInt(13) + 1;
+	int flower = random.nextInt(4);
+	System.out.print(number);
+	char c = 0;
+	switch (flower) {
+	case 0:
+		c = 'C';
+		break;
+	case 1:
+		c = 'H';
+		break;
+	case 2:
+		c = 'D';
+		break;
+	case 3:
+		c = 'S';
+		break;
+	}
+	System.out.println(c);
+
+}
+}
+}
+*/
